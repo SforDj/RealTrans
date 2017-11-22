504b01fce2fcd1fdaa3b34108b2f30a991ce3532
SforDj <1052360634@qq.com>
2017-11-22 19:29:51 +0800

    2017.11.22.1929 1.01

---
+++ data_handler/constant.py
@@ -0,0 +1,19 @@
+class Files:
+    VOC_FILE_DIRECTORY = "data/voc_files"
+    ENTITY_VOC_FILENAME = "entity_voc_list"
+    RELATION_VOC_FILENAME = "relation_voc_list"
+
+    DATA_FILE_DIRETORY = "data/data_files"
+    WIKI_SQ_FILENAME = "wiki_sq_entries"
+
+    PROCESSED_FILE_DIRECTORY = "data/processed_files"
+    WIKI_SQ_ID_FILENAME = "ids_wiki_sq"
+
+
+class NewVocConfig:
+    ENTITY_UNK = "E_UNK"
+    RELATION_UNK = "R_UNK"
+    MIN_ENTITY_APPEARANCE = 2
+    MIN_RELATION_APPEARANCE = 1
+
+
---
+++ data_handler/data_reader.py
@@ -0,0 +1,94 @@
+from data_handler.constant import Files
+from data_handler.constant import NewVocConfig
+
+import os
+import pickle
+
+
+def read_voc_list():
+    entity_voc_filename = os.path.join(Files.VOC_FILE_DIRECTORY, Files.ENTITY_VOC_FILENAME)
+    relation_voc_filename = os.path.join(Files.VOC_FILE_DIRECTORY, Files.RELATION_VOC_FILENAME)
+    entity_voc_list = dict()
+    relation_voc_list = dict()
+    with open(entity_voc_filename, 'r', encoding='UTF-8') as entity_input_file:
+        while True:
+            line = entity_input_file.readline()
+            if line == "":
+                break
+            line_array = line.replace("\n", "").split("\t")
+            if len(line_array) < 2:
+                break
+            elif len(line_array) == 2:
+                entity_voc_list[line_array[0]] = int(line_array[1])
+            else:
+                if int(line_array[2]) < NewVocConfig.MIN_ENTITY_APPEARANCE:
+                    continue
+                else:
+                    entity_voc_list[line_array[0]] = int(line_array[1])
+
+    with open(relation_voc_filename, 'r', encoding='UTF-8') as relation_inpit_file:
+        while True:
+            line = relation_inpit_file.readline()
+            if line == "":
+                break
+            line_array = line.replace("\n", "").split("\t")
+            if len(line_array) < 2:
+                break
+            elif len(line_array) == 2:
+                relation_voc_list[line_array[0]] = int(line_array[1])
+            else:
+                if int(line_array[2]) < NewVocConfig.MIN_RELATION_APPEARANCE:
+                    continue
+                else:
+                    relation_voc_list[line_array[0]] = int(line_array[1])
+
+    return entity_voc_list, relation_voc_list
+
+
+def build_ids(entity_voc_list, relation_voc_list):
+    id_file = os.path.join(Files.PROCESSED_FILE_DIRECTORY, Files.WIKI_SQ_ID_FILENAME)
+    raw_file = os.path.join(Files.DATA_FILE_DIRETORY, Files.WIKI_SQ_FILENAME)
+
+    if os.path.exists(id_file):
+        try:
+            with open(id_file, 'rb') as id_read:
+                data = pickle.load(id_read)
+                return data
+        except:
+            print("读取id异常\n")
+
+    data = []
+    with open(raw_file, 'rb') as raw_file_read:
+        lines = pickle.load(raw_file_read)
+        for line in lines:
+            head, relation, tail = line
+
+            if head in entity_voc_list:
+                head = entity_voc_list[head]
+            else:
+                head = entity_voc_list[NewVocConfig.ENTITY_UNK]
+
+            if relation in relation_voc_list:
+                relation = relation_voc_list[relation]
+            else:
+                relation = relation_voc_list[NewVocConfig.RELATION_UNK]
+
+            if tail in entity_voc_list:
+                tail = entity_voc_list[tail]
+            else:
+                tail = entity_voc_list[NewVocConfig.ENTITY_UNK]
+
+            entry = [head, relation, tail]
+            data.append(entry)
+
+    with open(id_file, 'wb') as id_file_write:
+        pickle.dump(data, id_file_write, True)
+
+    return data
+
+
+entity_voc_list, relation_voc_list = read_voc_list()
+data = build_ids(entity_voc_list, relation_voc_list)
+for i in range(0, 10):
+    print(data[i])
+
